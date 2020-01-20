d9ca35f6e2c73dda27df29db93f795a73e6c0474
Jimmy Vasquez <cjvasquez_98@hotmail.com>
2020-01-20 17:43:29 -0600

    update config

--- config/database.php
+++ config/database.php
@@ -1,17 +1,18 @@
 <?php
 class Database{
  
-    // specify your own database credentials
+    // Escribimos nuestras propias credenciales para el acceso a la BD
+    //Estos son los atributos de nuestra clase privados 
     private $host = "localhost";
-    private $db_name = "api_db";
+    private $db_name = "apidb";
     private $username = "root";
     private $password = "";
     public $conn;
  
-    // get the database connection
+    // Metodo para iniciar u obtener la conexión a la BD
     public function getConnection(){
  
-        $this->conn = null;
+        $this->conn = null;  //Apuntamos la variable conn a null, antes de hacer una conexión
  
         try{
             $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
